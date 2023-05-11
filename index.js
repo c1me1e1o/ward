879e187ee2b1e849d9e960637790439da772947f
Carlmelo Booc <car1melo@hotmail.com>
2023-05-11 07:53:56 -0400

    added fetch in js file

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 
 fetch("http://localhost:3000/colors")
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then((colors) => {
+        const container = document.querySelector("ul#colors")
+        colors.forEach(color => {
+            //name, hue, group
+            //create an element to hold the name // maybe h2, h3
+            //create element to hold description // p, maybe
+            //create a list item to hold the name and description
+            //append elements to list item
+            //append list item to container
+            
+
+        });
+    });
+
+
+
+
 
 // const colors = {
 //     "colors": 
