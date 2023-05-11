76c79d77969c271bc9f5ddce6fe787abc584b045
Carlmelo Booc <car1melo@hotmail.com>
2023-05-11 10:02:45 -0400

    added drawing functionality

--- index.js
+++ index.js
@@ -27,5 +27,29 @@ fetch("http://localhost:3000/colors")
     let is_drawing = false;
 
     canvas.addEventListener("touchstart", start, false);
+    canvas.addEventListener("touchmove", draw, false)
+    canvas.addEventListener("mousedown", start, false);
+    canvas.addEventListener("mousemove", draw, false)
 
 
+
+    function start(event) {
+        is_drawing = true;
+        context.beginPath();
+        context.moveTo(event.clientX - canvas.offsetLeft, 
+                       event.clientY - canvas.offsetTop);
+        event.preventDefault();
+    }
+
+
+    function draw(event) {
+        if ( is_drawing ) {
+            context.lineTo(event.clientX - canvas.offsetLeft, 
+                          event.clientY - canvas.offsetTop);
+            context.strokeStyle = draw_color;
+            context.lineWidth = draw_width;
+            context.lineCap = "round";
+            context.lineJoin = "round"   
+            context.stroke();
+        }
+    }
