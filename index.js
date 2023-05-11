1ad459b6237a23ba2fd7b1d73e5e4ef4ac37cb56
Carlmelo Booc <car1melo@hotmail.com>
2023-05-11 07:31:39 -0400

    index js works

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const colors = {
-    "colors": 
-      {
-        "color": "black",
-        "category": "hue",
-        "type": "primary",
-        "code": {
-          "rgba": [255,255,255,1],
-          "hex": "#000"
-        }
-      },
-      {
-        "color": "white",
-        "category": "value",
-        "code": {
-          "rgba": [0,0,0,1]
-          }
-        }
-    }
+console.log("index js works")
+
+// const colors = {
+//     "colors": 
+//       {
+//         "color": "black",
+//         "category": "hue",
+//         "type": "primary",
+//         "code": {
+//           "rgba": [255,255,255,1],
+//           "hex": "#000"
+//         }
+//       },
+//       {
+//         "color": "white",
+//         "category": "value",
+//         "code": {
+//           "rgba": [0,0,0,1]
+//           }
+//         }
+//     }
